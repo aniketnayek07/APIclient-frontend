87c465701497b5cca29d219647e02e2356982cd3
Aniket Nayek <142736800+aniketnayek07@users.noreply.github.com>
2024-09-03 14:29:38 +0530

    Some changes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
-import Url from "./components/Url";
+import Url from "@/components/Url";
+import Tab from "@/components/Tab";
 export default function InputWithButton() {
   return (
     <>
       <Header />
       <Url />
+      <Tab card="Param" />
     </>
   );
 }
